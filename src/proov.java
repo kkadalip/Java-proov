895984b88401b0013dfc910bdf644f40e91edcc3
Karl Kadalipp <Karl Kadalipp>
2016-01-11 20:59:09 +0200

    First commit.

---
+++ src/proov.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Date;
+
+public class proov {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.out.println(getLocalCurrentDate());
+	}
+
+	private static Date getLocalCurrentDate() {
+		return new Date();
+	}
+
+}
