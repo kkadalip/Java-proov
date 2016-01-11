fd7632c3662658b87f600be90b30db63d758449b
Karl Kadalipp <karl.kadalipp@gmail.com>
2016-01-11 21:51:55 +0200

    Printing out lines that were found in log file.

--- src/proov.java
+++ src/proov.java
@@ -1,5 +1,3 @@
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
@@ -11,17 +9,28 @@ public class proov {
 		// TODO Auto-generated method stub
 		System.out.println("test");
 		System.out.println(getLocalCurrentDate());
-		
+
 		String userDir = System.getProperty("user.dir"); // means C:\Users\karlk\workspace\Java-proov
 		//File file = new File("timing.log"); //("file.txt");
 		File file = new File(userDir + "/timing.log");
-	    try {
+
+		try {
 			Scanner scanner = new Scanner(file);
 			System.out.println("Scanner:" + scanner.toString());
+
+			// Reading Scanner class lines
+			int lineNr = 1;
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				System.out.println("Line: " + lineNr + " :" + line);
+				lineNr++;
+			}
 		} catch (FileNotFoundException e) {
 			System.out.println("Scanner error");
 			e.printStackTrace();
+
 		}
+
 	} // END MAIN
 
 	private static Date getLocalCurrentDate() {
