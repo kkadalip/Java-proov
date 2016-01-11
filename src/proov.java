63f4bcd4777cfbb3084dbb0245e038024ef37f0f
Karl Kadalipp <karl.kadalipp@gmail.com>
2016-01-11 22:45:36 +0200

    Printing out last splitted word of line aka duration.

--- src/proov.java
+++ src/proov.java
@@ -25,16 +25,21 @@ public class proov {
 				String line = scanner.nextLine();
 				System.out.println("Line: " + lineNr + " :" + line);
 				lineNr++;
+				String[] wordsOfLine = line.split(" ");
+				System.out.println("wordsOfLine length: " + wordsOfLine.length);
+				String lastWord = wordsOfLine[wordsOfLine.length - 1]; // duration
+				System.out.println("last word is " + lastWord);
+				// TODO Get request duration of each line (aka request)
+				// To do that. 1) Get last element of string (8 or 9 parts of string)
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("Scanner error");
 			e.printStackTrace();
-
 		}
 
 		long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		System.out.println("Program ran for " + totalTime + " milliseconds");
+		System.out.println("Program ran for " + totalTime + " milliseconds.");
 	} // END MAIN
 
 	private static Date getLocalCurrentDate() {
